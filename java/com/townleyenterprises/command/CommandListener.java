79e6321de4549c69d91a54b00b507809d641f6c5
atownley <atownley>
2004-01-25 18:33:33 +0000

    Fixed copyright year and creation date

--- java/com/townleyenterprises/command/CommandListener.java
+++ java/com/townleyenterprises/command/CommandListener.java
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2003, Andrew S. Townley
+// Copyright (c) 2003-2004, Andrew S. Townley
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
@@ -45,7 +45,7 @@ package com.townleyenterprises.command;
  * This interface must be implemented by classes that intend to
  * respond to command-line arguments processed by the CommandParser.
  *
- * @version $Id: CommandListener.java,v 1.1 2003/06/07 18:42:39 atownley Exp $
+ * @version $Id: CommandListener.java,v 1.2 2004/01/25 18:33:33 atownley Exp $
  * @author <a href="mailto:adz1092@netscape.net">Andrew S. Townley</a>
  * @since 2.0
  */
