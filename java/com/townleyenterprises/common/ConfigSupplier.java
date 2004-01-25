88069d37a6a526685ee4c067d5a4de0ff7f47d49
atownley <atownley>
2004-01-25 18:46:04 +0000

    Fixed copyright

--- java/com/townleyenterprises/common/ConfigSupplier.java
+++ java/com/townleyenterprises/common/ConfigSupplier.java
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2002-2003, Andrew S. Townley
+// Copyright (c) 2002-2004, Andrew S. Townley
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
@@ -45,7 +45,7 @@ package com.townleyenterprises.common;
  * This interface must be implemneted by applications that wish to use
  * the general configuration mechanism.
  *
- * @version $Id: ConfigSupplier.java,v 1.1 2003/06/07 18:42:30 atownley Exp $
+ * @version $Id: ConfigSupplier.java,v 1.2 2004/01/25 18:46:04 atownley Exp $
  * @author <a href="mailto:adz1092@netscape.net">Andrew S. Townley</a>
  */
 
