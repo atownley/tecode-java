b76838616ef73b49e1c15789d37ba1919d37209c
atownley <atownley>
2004-07-29 18:32:04 +0000

    Moved the resource stuff to be based on the new override things and made it more general as well

---
+++ java/com/townleyenterprises/common/ResourceLoader.java
@@ -0,0 +1,306 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2003-2004, Andrew S. Townley
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+//     * Redistributions of source code must retain the above
+//     copyright notice, this list of conditions and the following
+//     disclaimer.
+// 
+//     * Redistributions in binary form must reproduce the above
+//     copyright notice, this list of conditions and the following
+//     disclaimer in the documentation and/or other materials provided
+//     with the distribution.
+// 
+//     * Neither the names Andrew Townley and Townley Enterprises,
+//     Inc. nor the names of its contributors may be used to endorse
+//     or promote products derived from this software without specific
+//     prior written permission.  
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// File:	ResourceLoader.java
+// Created:	Wed Nov 12 14:06:28 GMT 2003
+//
+//////////////////////////////////////////////////////////////////////
+
+package com.townleyenterprises.common;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import javax.swing.ImageIcon;
+
+/**
+ * This class provides a way to access Java resource strings from a
+ * location relative to a specific class.  This class assumes that all
+ * resources are located in a sub-directory under the current package
+ * called 'resources'.  Thus, in Unix, the resource strings for
+ * <code>com.foo.MyClass</code> will be located in:
+ * <code>com/foo/resources/strings.properties</code> and the images
+ * will be located under <code>com/foo/resources/</code>.
+ * <p>
+ * This class supports a system property which will control how
+ * missing resources are reported.  The property is:
+ *
+ * <ul>
+ * <li>te-common.common.resourceloader.showmissingresources - set to
+ * <code>true</code> or <code>yes</code> to display messages about
+ * missing resources</li>
+ * </ul>
+ * </p>
+ *
+ * @version $Id: ResourceLoader.java,v 1.1 2004/07/29 18:32:04 atownley Exp $
+ * @author <a href="mailto:adz1092@yahoo.com">Andrew S. Townley</a>
+ */
+
+public class ResourceLoader implements ResourceProvider
+{
+	/**
+	 * The constructor takes an object to use as the basis for
+	 * finding resources.
+	 *
+	 * @param obj the object to provide the base resource location
+	 */
+
+	public ResourceLoader(Object obj)
+	{
+		this(obj.getClass());
+	}
+
+	/**
+	 * This constructor takes a class to use as the basis for
+	 * finding resources.
+	 *
+	 * @param cls the class providing the base resource location
+	 */
+
+	public ResourceLoader(Class cls)
+	{
+		this(cls, "strings");
+	}
+
+	/**
+	 * This version of the constructor is used to load resource
+	 * settings from an arbitrarily named resource property file.
+	 *
+	 * @param cls the class providing the base resource location
+	 * @param name the name of the resources
+	 */
+
+	public ResourceLoader(Class cls, String name)
+	{
+		this(cls, name, Locale.getDefault());
+	}
+
+	/**
+	 * This version of the constructor is used to load resource
+	 * settings from an arbitrarily named resource property file.
+	 *
+	 * @param cls the class providing the base resource location
+	 * @param name the name of the resources
+	 * @param locale the locale to use
+	 */
+
+	public ResourceLoader(Class cls, String name, Locale locale)
+	{
+		_klass = cls;
+		Package pkg = _klass.getPackage();
+		if(pkg != null)
+		{
+			_name = pkg.getName().concat(".resources.").concat(name);
+		}
+		else
+		{
+			_name = "resources.".concat(name);
+		}
+
+		_bundles.put(locale, 
+			ResourceBundle.getBundle(_name, locale));
+	}
+
+	/**
+	 * This method is used to find a given resource string.
+	 *
+	 * @param key the resource string key
+	 * @return the string for the key
+	 */
+
+	public String getString(String key)
+	{
+		return getBundleString(getResourceBundle(), key);
+	}
+
+	public String getString(String key, Locale locale)
+	{
+		// see if we have a bundle for the locale
+		ResourceBundle bundle = getResourceBundle(locale);
+		if(bundle == null)
+		{
+			// try to load it
+			bundle = ResourceBundle.getBundle(_name, locale);
+			_bundles.put(locale, bundle);
+		}
+
+		return getBundleString(bundle, key);
+	}
+
+	/**
+	 * This method is used to load an image for the default
+	 * locale.
+	 *
+	 * @param key the image key
+	 * @return the ImageIcon
+	 */
+
+	public ImageIcon getIcon(String key)
+	{
+		return getIcon(key, Locale.getDefault());
+	}
+
+	/**
+	 * This method is used to load an image for the specified
+	 * locale and key.
+	 *
+	 * @param key the image key
+	 * @param locale the locale
+	 * @return the ImageIcon
+	 */
+
+	public ImageIcon getIcon(String key, Locale locale)
+	{
+		// need to see if there's a locale-specific version of
+		// the icon.
+
+		String ext = Path.getExtension(key);
+		String name = "resources/".concat(Path.stripExtension(key));
+		String[] loc = ResourceManager.getResourceNames(name, locale);
+
+//System.out.println("loc.length:  " + loc.length);
+
+		URL url = null;
+		// find one that works
+		for(int i = 0; i < loc.length; ++i)
+		{
+//System.out.println("loc[" + i + "]:  " + loc[i].concat(".").concat(ext));
+			url = _klass.getResource(loc[i].concat(".").concat(ext));
+//System.out.println("trying url:  " + url);
+			if(url != null)
+				break;
+		}
+
+		return new ImageIcon(url);
+	}
+
+	public Collection getKeys()
+	{
+		ResourceBundle bndl = getResourceBundle();
+		ArrayList list = new ArrayList();
+		Enumeration e = bndl.getKeys();
+		while(e.hasMoreElements())
+		{
+			list.add(e.nextElement());
+		}
+
+		return list;
+	}
+
+	/**
+	 * This method returns the reference to the bundle for the
+	 * default locale.
+	 *
+	 * @return the bundle
+	 */
+
+	public ResourceBundle getResourceBundle()
+	{
+		return getResourceBundle(Locale.getDefault());
+	}
+
+	/**
+	 * This method is used to get a direct reference to the resource
+	 * bundle.
+	 *
+	 * @param locale the specific locale
+	 * @return the resource bundle
+	 */
+
+	public ResourceBundle getResourceBundle(Locale locale)
+	{
+		return (ResourceBundle)_bundles.get(locale);
+	}
+
+	/**
+	 * Provide some clue as to what we're doing and who we are.
+	 */
+
+	public String toString()
+	{
+		StringBuffer buf = new StringBuffer("[ResourceLoader (");
+		buf.append(hashCode());
+		buf.append("): klass='");
+		buf.append(_klass);
+		buf.append("'; name='");
+		buf.append(_name);
+		buf.append("' ]");
+
+		return buf.toString();
+	}
+
+	/**
+	 * This is a helper method since we now need to work with more
+	 * than one bundle.
+	 */
+
+	private String getBundleString(ResourceBundle rez, String key)
+	{
+		String s = null;
+
+		try
+		{
+			s = rez.getString(key);
+		}
+		catch(MissingResourceException e)
+		{
+			String p = System.getProperty("te-common.common.resourceloader.showmissingresources");
+			if(p != null && (p.toLowerCase().charAt(0) == 'y'
+					|| p.toLowerCase().charAt(0) == 't'))
+			{
+				System.err.println(Strings.format("fMissingResource", new Object[] { getClass().getName(), e.getKey(), _name }));
+//				e.printStackTrace();
+			}
+		}
+
+		return s;
+	}
+
+	/** a reference to the class */
+	private Class			_klass;
+
+	/** save a reference to our resource bundle name */
+	private String			_name;
+
+	/** the resource bundles for the other locales */
+	private HashMap			_bundles = new HashMap();
+}
---
+++ java/com/townleyenterprises/common/ResourceManager.java
@@ -0,0 +1,267 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2004, Andrew S. Townley
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+//     * Redistributions of source code must retain the above
+//     copyright notice, this list of conditions and the following
+//     disclaimer.
+// 
+//     * Redistributions in binary form must reproduce the above
+//     copyright notice, this list of conditions and the following
+//     disclaimer in the documentation and/or other materials provided
+//     with the distribution.
+// 
+//     * Neither the names Andrew Townley and Townley Enterprises,
+//     Inc. nor the names of its contributors may be used to endorse
+//     or promote products derived from this software without specific
+//     prior written permission.  
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// File:	ResourceManager.java
+// Created:	Thu Jul 29 11:14:58 IST 2004
+//
+//////////////////////////////////////////////////////////////////////
+
+package com.townleyenterprises.common;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javax.swing.ImageIcon;
+
+/**
+ * This class provides a way of managing resource hierarchies
+ * independent of the locale of a resource bundle.  The default
+ * resolution strategy is an instance of {@link
+ * com.townleyenterprises.common.UseLastOverrideStrategy} which should
+ * "do the right thing" in almost all of the cases.
+ *
+ * @version $Id: ResourceManager.java,v 1.1 2004/07/29 18:32:04 atownley Exp $
+ * @author <a href="mailto:adz1092@yahoo.com">Andrew S. Townley</a>
+ */
+
+public class ResourceManager extends OverrideManager
+		implements ResourceProvider
+{
+	/**
+	 * This method is used to return the possible variants of a
+	 * given base resource name for the given locale.
+	 *
+	 * @param base the base name
+	 * @param locale the locale
+	 * @return an array of possible locations for the resource, in
+	 * order of most specialized to least specialized.
+	 */
+
+	public static String[] getResourceNames(String base, Locale locale)
+	{
+		ArrayList list = new ArrayList();
+		String language = locale.getLanguage();
+		String country = locale.getCountry();
+		String variant = locale.getVariant();
+
+		list.add(base.concat("_").concat(locale.toString()));
+		if(!"".equals(variant))
+		{
+			list.add(base.concat("_").concat(language.concat("_").concat(country)));
+		}
+		
+		if(!"".equals(country))
+		{
+			list.add(base.concat("_").concat(language));
+		}
+
+		// always add the default
+		list.add(base);
+
+		return (String[])list.toArray(new String[list.size()]);
+	}
+
+	/**
+	 * This method sets up the default resolution strategy.
+	 */
+
+	public ResourceManager()
+	{
+		setOverrideStrategy(new UseLastOverrideStrategy());
+	}
+
+	public String getString(String key)
+	{
+		return (String)get(key);
+	}
+
+	public String getString(String key, Locale locale)
+	{
+		OverrideNode on = getNode(key);
+		if(on == null)
+			return null;
+
+		ResourceProvider rp = (ResourceProvider)on.get();
+
+		if(rp == null)
+			return null;
+
+		return rp.getString(key, locale);
+	}
+
+	public ImageIcon getIcon(String key)
+	{
+		return getIcon(key, Locale.getDefault());
+	}
+
+	public ImageIcon getIcon(String key, Locale locale)
+	{
+		return findIcon(key, locale);
+	}
+
+	/**
+	 * This method keeps track of the objects managed so that the
+	 * icons can be resolved in the right order.
+	 */
+
+	public void manage(Object object)
+	{
+		super.manage(object);
+		_iconRoot.set(resolve("___icons", 
+				_iconRoot.get(), object));
+	}
+
+	/**
+	 * This method is used to add a resource provider.  It simply
+	 * deferrs to the {@link #manage} method, but it provides some
+	 * type checking.
+	 *
+	 * @param provider the ResourceProvider
+	 */
+
+	public void addProvider(ResourceProvider provider)
+	{
+		manage(provider);
+	}
+
+	/**
+	 * This method is used to format a given message located in
+	 * the resource bundle.  The message is first retrieved and
+	 * then processed by an instance of the MessageFormat class.
+	 *
+	 * @param key the message key
+	 * @param locale the locale
+	 * @param args the format arguments
+	 */
+
+	public String format(String key, Locale locale, Object[] args)
+	{
+		String fmt = getString(key, locale);
+		MessageFormat format = new MessageFormat(fmt, locale);
+		return format.format(args, 
+				new StringBuffer(), null).toString();
+	}
+
+	/**
+	 * This method is used to format a given message located in
+	 * the resource bundle.  The message is first retrieved and
+	 * then processed by an instance of the MessageFormat class.
+	 *
+	 * @param key the message key
+	 * @param args the format arguments
+	 */
+
+	public String format(String key, Object[] args)
+	{
+		return format(key, Locale.getDefault(), args);
+	}
+
+	/**
+	 * This method is used to retrieve the property keys from the
+	 * specified object.  The current assumption is that all
+	 * objects managed by this class will at least conform to a
+	 * common interface.
+	 *
+	 * @param object the object to manage
+	 * @return a collection of key values
+	 */
+
+	protected Collection getKeys(Object object)
+	{
+		if(object instanceof ResourceProvider)
+		{
+			return ((ResourceProvider)object).getKeys();
+		}
+
+		return Collections.EMPTY_SET;
+	}
+
+	/**
+	 * This method is used to retrieve the value for the specific
+	 * property key given the managed object.
+	 *
+	 * @param key the key to retrieve
+	 * @param object the object to manipulate
+	 * @return the value for the key
+	 */
+
+	protected Object getValue(Object key, Object object)
+	{
+		if(object instanceof ResourceProvider)
+		{
+			return ((ResourceProvider)object).getString(key.toString());
+		}
+
+		return null;
+	}
+
+	/**
+	 * This method is used to walk the entire set of providers to
+	 * attempt to find the icon resource.
+	 *
+	 * FIXME:  this isn't terribly efficient, but...
+	 */
+
+	private ImageIcon findIcon(String key, Locale locale)
+	{
+		OverrideNode node = _iconRoot;
+		ResourceProvider rp = (ResourceProvider)node.get();
+		if(rp == null)
+			return null;
+
+		ImageIcon icon = rp.getIcon(key, locale);
+		while(icon == null)
+		{
+			// walk the list
+			node = node.getPrevious();
+			rp = (ResourceProvider)node.get();
+			if(rp == null)
+				return null;
+
+			icon = rp.getIcon(key, locale);
+		}
+
+		return icon;
+	}
+
+	/** maintain a node to track the correct resolution order */
+	private OverrideNode	_iconRoot = new OverrideNode();
+}
